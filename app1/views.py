305bd86a6437d6958d032fa3df982677cc41d8d7
dileepravan6666 <dileepkomarla@gmail.com>
2022-06-21 18:38:15 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from unicodedata import name
+from django.shortcuts import render
+
+# Create your views here.
+def jinja_print(request):
+    d={'name':'dillu','age':2}
+    return render(request,'jinja_print.html',context=d)
+
